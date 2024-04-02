b101db0cc60f3c5751446dc9de91f1aadb25890d
pieter-andries <pieterandries@icloud.com>
2024-04-02 14:17:01 +0200

    added preserved state when going to details and navigate back to genres games are still there.

--- src/pages/Genres/Genres.jsx
+++ src/pages/Genres/Genres.jsx
@@ -7,11 +7,15 @@ import { fetchGamesByGenre, fetchGenres } from "../../helpers/api.js";
 import GenreCard from "../../components/GenreCard/GenreCard.jsx";
 import GameList from "../../components/GamesList/GameList.jsx";
 import { useQuery } from "react-query";
+import { useLocation, useNavigate } from "react-router-dom";
 
 //TODO: Check of cache wel echt werkt voor api calls
 
 const Genres = () => {
   const [selectedGenreId, setSelectedGenreId] = useState(null);
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const {
     isLoading,
     isError,
@@ -27,11 +31,14 @@ const Genres = () => {
       enabled: false,
       staleTime: 1000 * 60 * 60 * 5,
       cacheTime: 1000 * 60 * 60 * 5,
+      initialData: location.state?.selectedGenreGames,
     }
   );
 
   const handleGenreClick = async (genreId) => {
     setSelectedGenreId(genreId);
+    await refetchGamesByGenre();
+    navigate(location.pathname, { state: { selectedGenreGames } });
   };
 
   useEffect(() => {
